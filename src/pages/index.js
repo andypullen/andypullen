687e3aaadc5df9a30af13228cbd2bc64fb0e4d2a
Andy Pullen <ajpullen@me.com>
2021-02-07 19:46:56 +0000

    skew

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,9 @@ import React from "react"
 
 export default function Home() {
   return <main>
-    <h1>Andy Pullen</h1>
-    <p><a href="mailto:a@andypullen.com">email me</a></p>
-    </main>
+    <div>
+      <h1>Andy Pullen</h1>
+      <p><a href="mailto:a@andypullen.com">email me</a></p>
+    </div>
+  </main>
 }
