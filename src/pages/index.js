8d85de33ec404fb069b70229b2824336440b1033
Andy Pullen <ajpullen@me.com>
2021-02-07 19:22:24 +0000

    style making

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,8 @@
 import React from "react"
 
 export default function Home() {
-  return <div>
+  return <main>
     <h1>Andy Pullen</h1>
     <p><a href="mailto:a@andypullen.com">email me</a></p>
-    </div>
+    </main>
 }
