b4159b1a121a2d8f842ad80610db3040c9e72130
Andy Pullen <andypullen@users.noreply.github.com>
2021-04-30 19:46:04 -0700

    added work page

---
+++ src/pages/work.js
@@ -0,0 +1,21 @@
+import React from "react"
+
+export default function Home() {
+  return <main>
+    <div className="content">
+      <h1>Work I'm proud of</h1>
+      <p>
+        <a href="https://www.summitbirthportland.com">Summit Birth Services</a> – branding & Squarespace website
+      </p>
+      <p>
+        <a href="https://cea.llc">Construction Engineering Associates</a> – branding & Squarespace website
+      </p>
+      <p>
+        <a href="https://gsmuganda.org">Good Samaritan Uganda</a> – Branding
+      </p>
+      <h3>My day job</h3>
+      <p>
+        <a href="https://willamette.cc/>Willamette Christian Church</a> - Web design & development using <a href="https://craftcms.org">Craft CMS</a>
+    </div>
+  </main>
+}
