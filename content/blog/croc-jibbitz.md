6afed119b84edee05d03456f40cd861569f0d5cf
andypullen <ajpullen@me.com>
2023-04-22 14:30:40 +0000

    11ty!

---
+++ content/blog/croc-jibbitz.md
@@ -0,0 +1,9 @@
+---
+title: Croc Jibbitz
+date: 2023-02-03
+snippet: Who knew these were a thing?
+---
+
+I came across this [toe jibbit for crocs](https://www.etsy.com/listing/1374331352/croc-toe-jibbit), and much laughter ensued, and then found out that there is a [whole](https://www.etsy.com/search?q=croc%20jibbitz&ref=auto-1&as_prefix=croc%20jib) [swath](https://www.crocs.com/c/jibbitz) of jibbitz for crocs. Are there jibbitz for other things? What does jibbitz mean? Do people still wear crocs? Do people who still wear crocs actually decorate them with jibbitz? What about toe jibbitz? These are important questions!
+
+This kid is a genius.
---
+++ tailwind.config.js
@@ -0,0 +1,49 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "assets/**/*.svg",
+    "_includes/**/*.{html,njk,md}",
+    "_site/**/*.{html,njk,md}",
+  ],
+  theme: {
+    colors: {
+      'blue': '#62cdff',
+      'blue-dark': '#2b303f',
+      'purple': '#7e5bef',
+      'pink': '#ff49db',
+      'orange': '#ff7849',
+      'green': '#6ce879',
+      'yellow': '#ffc82c',
+      'gold': '#ffab62',
+      'gray-dark': '#273444',
+      'gray': '#8492a6',
+      'gray-light': '#d3dce6',
+    },
+    fontFamily: {
+      sans: ['sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    extend: {
+      spacing: {
+        '8xl': '96rem',
+        '9xl': '128rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      }
+    },
+    fontWeight: {
+      light: 200,
+      normal: 400,
+      bold: 600,
+    }
+  },
+  safelist: [
+    {
+      pattern: /pl-\d+/,
+      variants: ['md'],
+    },
+  ],
+  plugins: [],
+}
+
