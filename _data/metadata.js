e291b71d953b6bd3c76fcf34cd55c4607c700cf5
andypullen <ajpullen@me.com>
2023-04-23 13:51:04 +0000

    added feeds & descriptions

---
+++ _data/metadata.js
@@ -0,0 +1,11 @@
+module.exports = {
+	title: "The Andy Blog",
+	url: "https://andypullen.com/blog/",
+	language: "en",
+	description: "The Hypertext Home of Andy Pullen",
+	author: {
+		name: "Andy Pullen",
+		email: "a@andypullen.com",
+		url: "https://andypullen.com"
+	}
+}
